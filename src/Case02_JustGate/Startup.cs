56e8009798eecb5eb8f48f19a95b21b62f9864ac
Louis DeJardin <lodejard@microsoft.com>
2012-05-04 21:41:45 -0700

    Adding a few initial case studies

---
+++ src/Case02_JustGate/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Gate;
+using Owin;
+
+namespace Case02_JustGate
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.RunDirect(
+                (req, res) =>
+                {
+                    res.ContentType = "text/plain";
+                    res.Write("You did a {0} at {1}", req.Method, req.Path);
+                    res.End();
+                });
+        }
+    }
+}
---
+++ src/Case03_JustOwin/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Owin;
+
+namespace Case03_JustOwin
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.Use<AppDelegate>(_ => App);
+        }
+
+        private void App(IDictionary<string, object> env, ResultDelegate result, Action<Exception> fault)
+        {
+            var data = new ArraySegment<byte>(Encoding.UTF8.GetBytes(string.Format(
+                "You did a {0} at {1}",
+                env["owin.RequestMethod"],
+                env["owin.RequestPath"])));
+
+            result(
+                "200 OK",
+                new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Content-Type", new[] {"text/plain"}}
+                },
+                (write, flush, end, cancellationToken) =>
+                {
+                    write(data);
+                    end(null);
+                });
+        }
+    }
+}
